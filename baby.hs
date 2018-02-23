56810a17606b1ad28ffcd967491e933345502b72
jiangjianlei <jiang3.jl@gmail.com>
2018-02-23 18:13:39 +0800

    learn list

--- baby.hs
+++ baby.hs
@@ -4,6 +4,8 @@ doubleUs x y = doubleMe x + doubleMe y
 
 doubleSmallNumber x = if x > 100 then x else doubleMe x
 
-conanO'Brien = "It's a-me, Conan O'Brien!"
+conanOBrien = "It's a-me, Conan O'Brien!"
 
 doubleBigNumber x = if x > 100 then doubleMe x else x
+
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]   
